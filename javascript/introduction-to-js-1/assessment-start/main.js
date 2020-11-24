3600f8a04acb37f4ecfa6462d9a4fab859cdd4e6
Cynthia Trujillo <developercynthia@gmail.com>
2020-11-23 18:48:03 -0600

    Silly story generator

--- javascript/introduction-to-js-1/assessment-start/main.js
+++ javascript/introduction-to-js-1/assessment-start/main.js
@@ -7,32 +7,43 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = `It was 94 fahrenheit outside, so ${insertX} went for a walk. When they got to ${insertY}, they stared in horror for a few moments, then ${insertZ}. Bob saw the whole thing, but was not surprised — ${insertX} weighs 300 pounds, and it was a hot day.`;
-const insertX = `Willy the Goblin
-    Big Daddy
-    Father Christmas`;
-const insertY = `the soup kitchen
-    Disneyland
-    the White House`;
-const insertZ = `spontaneously combusted
-    melted into a puddle on the sidewalk
-    turned into a slug and crawled away`;
+let storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`;
+let insertX = ['Willy the Goblin',
+    'Big Daddy',
+    'Father Christmas'];
+let insertY = ['the soup kitchen',
+    'Disneyland',
+    'the White House'];
+let insertZ = ['spontaneously combusted',
+    'melted into a puddle on the sidewalk',
+    'turned into a slug and crawled away'];
 
-    randomize.addEventListener('click', result);
+randomize.addEventListener('click', result);
 
-    function result() {
-    
+function result() {
+    let newStory = storyText;
+
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+
       if(customName.value !== '') {
-        let name = customName.value;
-    
+        const name = customName.value;
+        newStory = newStory.replace('Bob', name);
       }
     
       if(document.getElementById("uk").checked) {
-        let weight = Math.round(300);
-        let temperature =  Math.round(94);
-    
+        const weight = `${Math.round((300/14))} stone`;
+        const temperature =  `${Math.round((94 - 32) * 5/9)} centigrade`;
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 fahrenheit', temperature);
       }
     
-      story.textContent = ;
+      story.textContent = newStory;
       story.style.visibility = 'visible';
     }
